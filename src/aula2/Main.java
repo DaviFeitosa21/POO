3fa2a74cec4c2ffb07fdec569c995c3a86e269e4
DaviFeitosa <90164085+DaviFeitosa21@users.noreply.github.com>
2025-03-17 15:01:41 -0300

    exercicio 2

--- src/aula2/Main.java
+++ src/aula2/Main.java
@@ -3,7 +3,6 @@ package aula2;
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
@@ -31,5 +30,4 @@ public class Main {
 		
 		sc.close();
 	}
-	
 }
---
+++ src/aula2/Main2.java
@@ -0,0 +1,23 @@
+package aula2;
+
+import java.util.Scanner;
+
+public class Main2 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int numeroDigitado;
+		
+		System.out.println("Digite um número:");
+		numeroDigitado = sc.nextInt();
+		
+		
+		for(int i = 0; i <= numeroDigitado; i++) {
+			i += i;
+			System.out.println(i);
+		}
+		
+		sc.close();
+	}
+}
