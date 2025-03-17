afc6b9bbe4acb79fa74cb48ad669f32f5243295a
DaviFeitosa <davisfeitosa21@gmail.com>
2025-03-17 00:38:12 -0300

    numeros primos

--- src/poo/Main.java
+++ src/poo/Main.java
@@ -1,9 +1,40 @@
 package poo;
 
+import java.util.Scanner;
+
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.print("Teste");
+		Scanner sc = new Scanner(System.in);
+		
+		int numeroPrimo;
+		boolean primo = true;;
+		
+		System.out.println("Digite um número para verficar se é primo:");
+		numeroPrimo = sc.nextInt();
+		
+		if(numeroPrimo <= 1) {
+			primo = false;
+		}
+		
+		if(numeroPrimo > 1) {
+			for(int i = 2; i <= Math.sqrt(numeroPrimo); i++) {
+				if(numeroPrimo % i == 0) {
+					primo = false;
+					break;
+				}
+			}
+		}
+		
+		if(primo) {
+			System.out.println("Número é primo!");
+		}
+		else {
+			System.out.println("Número não é primo!");
+		}
+		
+		
+		sc.close();
 	}
 
 }
